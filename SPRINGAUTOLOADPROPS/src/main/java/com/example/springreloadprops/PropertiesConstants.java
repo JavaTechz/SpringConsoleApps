d77779a7c6e81f62ca3f67621a9367d134308b0f
styphinsontoms <styphinsontoms@gmail.com>
2016-09-28 10:09:25 +0530

    initial commit

---
+++ SPRINGAUTOLOADPROPS/src/main/java/com/example/springreloadprops/PropertiesConstants.java
@@ -0,0 +1,20 @@
+package com.example.springreloadprops;
+
+
+
+public class PropertiesConstants {
+
+	public static final String PROPERTIES_FILE_PATH ="application.properties";
+
+	public static final int REFRESH_DELAY = 1000;
+
+	public static final String MODE = "mode";
+	public static final String HOST = "host";
+	public static final String PORT = "port";
+	public static final String USER = "user";
+	public static final String PASSWORD = "password";
+
+	public static final String ACTIVE_MODE = "active";
+	public static final String IDLE_MODE = "idle";
+
+}
